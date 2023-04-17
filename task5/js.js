65d4fd1f7c13c0d114f909f381551b6fec0a39e1
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-04-17 13:42:11 +0800

    finished the tsks 5

---
+++ task5/js.js
@@ -0,0 +1,9 @@
+ function sixToast(num) {
+        var tost=6
+        var howManyNeed=tost-num
+        if (howManyNeed<0){
+            howManyNeed=howManyNeed*-1
+        }
+        return howManyNeed
+    }
+  sixToast(17)
