de57d05273f7de8513b73bb258edd44049603828
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-04-19 21:18:42 +0800

    finished the task 15

---
+++ task16/index.js
@@ -0,0 +1,15 @@
+function bmi(weight, height) {
+  var index=weight/(height*height)
+  if(index<= 18.5){
+    return "Underweight"
+  }
+  if(index<= 25.0 ){
+    return "Normal"
+  }
+  if(index<= 30.0 ){
+    return "Overweight"
+  }
+  if(index > 30 ){
+    return "Obese"
+  }
+};
