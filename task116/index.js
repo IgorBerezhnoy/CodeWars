0b3ff6f85530d8bf39577623acde0b8b251d6c7f
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-08-22 01:42:52 +0800

    собес

---
+++ task116/index.js
@@ -0,0 +1,15 @@
+let arr=["1","2","3"]
+
+console.log(arr.slice(1,3))
+console.log(arr.splice(1,3))
+
+// for in for off
+// Set Map
+// связаные списки
+// де файн пропертиз
+//статические и нестатические
+// стоп прапогейшен
+
+
+console.log(NaN==NaN);
+console.log(NaN===NaN);
