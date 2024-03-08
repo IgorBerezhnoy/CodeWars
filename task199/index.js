57b8f38ea6aef9ecbe453e8b60e5b3481c37b697
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-03-09 00:43:18 +0800

    finished 200-202

--- task199/index.js
+++ task199/index.js
@@ -1,1 +1,23 @@
-const combineNames = (firstName, lastName) => `${firstName} ${lastName}`;
+function add(a,b){
+  return a+b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function mod(a,b){
+  return a%b
+}
+
+function exponent(a,b){
+  return a**b
+}
+
+function subt(a,b){
+  return a-b
+}
